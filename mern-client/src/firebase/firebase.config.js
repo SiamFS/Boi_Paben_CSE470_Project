2434169087611fe5241483f45119a5309b7eaf40
SiamFS <siamferdous1@gmail.com>
2024-10-15 02:15:44 +0600

    fix

--- mern-client/src/firebase/firebase.config.js
+++ mern-client/src/firebase/firebase.config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC4mJg-Hu2Xi9RK86EoFt5s6yp_kZUODX8",
-  authDomain: "mern-book-inventory-765aa.firebaseapp.com",
-  projectId: "mern-book-inventory-765aa",
-  storageBucket: "mern-book-inventory-765aa.appspot.com",
-  messagingSenderId: "788530059359",
-  appId: "1:788530059359:web:1596ea7745c09f956323ea"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
