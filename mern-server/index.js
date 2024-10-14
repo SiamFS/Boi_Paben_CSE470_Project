e17fa4daa288cbcb6aaf173e9e4da290867cfd13
SiamFS <siamferdous1@gmail.com>
2024-10-15 01:30:22 +0600

    fix

--- mern-server/index.js
+++ mern-server/index.js
@@ -7,7 +7,10 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://boi-paben-frontend.onrender.com',
+  credentials: true,
+}));
 app.use(express.json());
 
 // MongoDB configuration
